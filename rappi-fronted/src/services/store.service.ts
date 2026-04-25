fe6ab321770b8c323d69c7ecfeb99d017e664d19
alejandraemperador <alejandraemperadorg@gmail.com>
2026-04-25 08:15:51 -0500

    fix services

--- rappi-fronted/src/services/store.service.ts
+++ rappi-fronted/src/services/store.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://rappi-lab-4.vercel.app/api";
+const API_URL = `${import.meta.env.VITE_API_URL}api`;
 
 const getAuthHeaders = () => ({
     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
